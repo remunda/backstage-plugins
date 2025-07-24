29b0767646e5e3ce75b0749118c3fc5d9d3067a1
Jan Remunda <jan.remunda@notino.com>
2025-07-24 12:06:42 +0200

    new plugins from template, manually copied

---
+++ plugins/frontend/src/index.ts
@@ -0,0 +1,1 @@
+export { datacontractPlugin, datacontractPage } from "./plugin";
---
+++ plugins/frontend/src/plugin.test.ts
@@ -0,0 +1,7 @@
+import { datacontractPlugin } from './plugin';
+
+describe('backstage-plugin-datacontract', () => {
+  it('should export plugin', () => {
+    expect(datacontract).toBeDefined();
+  });
+});

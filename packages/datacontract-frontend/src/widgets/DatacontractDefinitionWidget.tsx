9a4b670d2d4d7d3fd249bd93389a410f9ebbffc3
Jan Remunda <jan.remunda@notino.com>
2025-07-24 10:51:53 +0200

    remove ai crap

--- packages/datacontract-frontend/src/widgets/DatacontractDefinitionWidget.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { CodeSnippet } from '@backstage/core-components';
-
-export const DatacontractDefinitionWidget = ({ definition }: { definition: string }) => (
-  <CodeSnippet text={definition} language="yaml" showLineNumbers />
-);
